f2a2a5128bfa8ce0ce09d83f14e4e2b3b4d54dfb
Gashkov Saveliy <120210549+Nemo121007@users.noreply.github.com>
2024-10-06 18:46:27 +0300

    Изменение

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,15 +1,15 @@
 import unittest
-import qadro_calculate
+from qadro_calculate import qadro
 
 class TestCalculator(unittest.TestCase):
 
     def test_add_positive(self):
-        result = qadro_calculate.qadro(2)
+        result = qadro(2)
         self.assertEqual(result, 4)
 
 
     def test_add_negative(self):
-        result = qadro_calculate.qadro(-1)
+        result = qadro(-1)
         self.assertEqual(result, 1)
 
 if __name__ == '__main__':
